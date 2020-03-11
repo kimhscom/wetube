3e24916ebd3b786bda50bb94e128d7aded1d4787
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-03-11 14:15:14 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+    console.log(req);
+    res.send("Hi from home!!");
+}
+
+function handleProfile(req, res) {
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
 
 
