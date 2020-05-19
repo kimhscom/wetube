b00db0f9f5887220face6d4e14076060fe62b06c
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-05-19 19:28:40 +0900

    npm packages update

--- db.js
+++ db.js
@@ -4,13 +4,15 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
