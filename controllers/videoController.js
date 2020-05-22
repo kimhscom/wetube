c917d2a6048b955059b05961071b3ef0ff995d82
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-05-22 19:50:29 +0900

    API Adding a Comment part One

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 // Home
 
@@ -60,7 +61,9 @@ export const videoDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const video = await Video.findById(id).populate("creator");
+    const video = await Video.findById(id)
+      .populate("creator")
+      .populated("comments");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     console.log(error);
@@ -129,6 +132,29 @@ export const postResiterView = async (req, res) => {
     video.views += 1;
     video.save();
     res.status(200);
+  } catch (error) {
+    res.status(400);
+  } finally {
+    res.end();
+  }
+};
+
+// Add Comment
+
+export const postAddComment = async (req, res) => {
+  const {
+    params: { id },
+    body: { comment },
+    user,
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text: comment,
+      creator: user.id,
+    });
+    video.comments.push(newComment.id);
+    video.save();
   } catch (error) {
     res.status(400);
   } finally {
--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
 import routes from "../routes";
-import { postResiterView } from "../controllers/videoController";
+import {
+  postResiterView,
+  postAddComment,
+} from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post(routes.registerView, postResiterView);
+apiRouter.post(routes.addComment, postAddComment);
 
 export default apiRouter;
