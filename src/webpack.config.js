1709a0caa6a4e785eb6bcda8503bc23175dfae4d
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-05-25 15:56:59 +0900

    Add favicon

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
@@ -15,36 +16,39 @@ const config = {
         test: /\.(js)$/,
         use: [
           {
-            loader: "babel-loader"
-          }
-        ]
+            loader: "babel-loader",
+          },
+        ],
       },
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
           {
-            loader: "css-loader"
+            loader: "css-loader",
           },
           {
             loader: "postcss-loader",
             options: {
               plugins() {
                 return [autoprefixer({ overrideBrowserslist: "cover 99.5%" })];
-              }
-            }
+              },
+            },
           },
           {
-            loader: "sass-loader"
-          }
-        ])
-      }
-    ]
+            loader: "sass-loader",
+          },
+        ]),
+      },
+    ],
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].js"
+    filename: "[name].js",
   },
-  plugins: [new ExtractCSS("styles.css")]
+  plugins: [
+    new ExtractCSS("styles.css"),
+    new FaviconsWebpackPlugin("./assets/favicon.png"),
+  ],
 };
 
 module.exports = config;
