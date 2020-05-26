b204725de7d64c78896ae41e43cae351c1e5606e
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-05-26 19:58:28 +0900

    Modify set total time

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -147,7 +147,9 @@ function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
-  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+  window.onload = () => {
+    setTotalTime();
+  };
   videoPlayer.addEventListener("ended", handleEnded);
   volumeRange.addEventListener("input", handleDrag);
 }
